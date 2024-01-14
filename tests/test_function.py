fbfb87c84fe914580ee0d3ba39890dca43845755
Artur Khamidulliun <artrimskiy@gmail.com>
2024-01-14 12:12:32 +0500

    add test_sorted_by_date

--- tests/test_function.py
+++ tests/test_function.py
@@ -4,4 +4,11 @@ def test_filter_list():
     assert filter_list([{"state": "EXECUTED"}]) == [{"state": "EXECUTED"}]
     assert filter_list([{}]) == []
 
+def test_sorted_by_date():
+    assert sorted_by_date([{"date": "2019-07-03T18:35:29.512364"},
+                           {"date": "2018-06-30T02:08:58.425572"},
+                           {"date": "2019-08-26T10:50:58.294041"}]) == [{"date": "2019-08-26T10:50:58.294041"},
+                                                                        {"date": "2019-07-03T18:35:29.512364"},
+                                                                        {"date": "2018-06-30T02:08:58.425572"}]
+
 
